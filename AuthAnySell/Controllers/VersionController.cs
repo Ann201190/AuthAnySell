5f064cc1f169051d778735eb5a675ddeb39f1108
Ann201190 <74066708+Ann201190@users.noreply.github.com>
2022-06-23 16:33:48 +0300

    version;

---
+++ AuthAnySell/Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace AuthAnySell.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public VersionController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Route("unityminversion")]
+        [HttpGet]
+        public async Task<IActionResult> GetMinVersion()
+        {
+            return Ok(_config.GetValue<double>("UnitySettings:MinVersion"));
+        }
+    }
+}
